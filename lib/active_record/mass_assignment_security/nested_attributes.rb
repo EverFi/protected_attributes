297cd1de5bf558e482cc85fa43ee562b62b84d48
Colin Van Dyke <vandyke.colin@gmail.com>
2016-01-06 09:09:47 -0500

    Avoid double callbacks in Rails >= 4.1
    See rails#77

--- lib/active_record/mass_assignment_security/nested_attributes.rb
+++ lib/active_record/mass_assignment_security/nested_attributes.rb
@@ -15,6 +15,11 @@ module ActiveRecord
 
           attr_names.each do |association_name|
             if reflection = reflect_on_association(association_name)
+              if active_record_40?
+                reflection.options[:autosave] = true
+              else
+                reflection.autosave = true
+              end              
               reflection.options[:autosave] = true
               add_autosave_association_callbacks(reflection)
 
